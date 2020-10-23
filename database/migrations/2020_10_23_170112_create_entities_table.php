27ff1cfb3c4a74aab93c203685cb2175312ca7b6
jacksonsr45 <jacksonsr45@gmail.com>
2020-10-23 14:05:52 -0300

    feat(Add-new-migrate-Entities): Creating new migrate Entities
    jacksonsr45

---
+++ database/migrations/2020_10_23_170112_create_entities_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entities', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('type');
+            $table->string('description');
+            $table->boolean('is_active')->default(false);
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')
+                                                        ->onDelete('cascade')
+                                                        ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entities');
+    }
+}
