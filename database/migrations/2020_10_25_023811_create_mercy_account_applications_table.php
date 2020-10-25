e1557492c0f9fab084c6aad34166cb34a369d861
jacksonsr45 <jacksonsr45@gmail.com>
2020-10-24 23:47:06 -0300

    feat(Add-migration-from-all-tables): Add tables
    jacksonsr45

---
+++ database/migrations/2020_10_25_023811_create_mercy_account_applications_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMercyAccountApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mercy_account_applications', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('entity_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('account_id');
+            $table->string('historic');
+            $table->float('deb', 10, 2);
+            $table->float('cred', 10, 2);
+            $table->float('total', 10, 2);
+            $table->timestamps();
+            $table->foreign('entity_id')->references('id')->on('entities')
+                                                        ->onDelete('cascade')
+                                                        ->onUpdate('cascade');
+            $table->foreign('user_id')->references('id')->on('users')
+                                                        ->onDelete('cascade')
+                                                        ->onUpdate('cascade');
+            $table->foreign('account_id')->references('id')->on('bank_accounts')
+                                                        ->onDelete('cascade')
+                                                        ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mercy_account_applications');
+    }
+}
